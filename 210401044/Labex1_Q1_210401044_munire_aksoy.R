c2c25ad30429bc9556d83b9da1efc50638723fde
Minnoi <munireaksoy2002@gmail.com>
2023-11-16 17:54:03 +0300

    1.5 testleriyle tamamlandı.

---
+++ 210401044/Labex1_Q1_210401044_munire_aksoy.R
@@ -0,0 +1,11 @@
+library (readr)
+
+
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+dosya <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = dosya, method = "auto")
+maps <- read.csv(dosya, header=TRUE, sep=";")
+maps$Latitude <- as.numeric(gsub("N", "" , maps$Latitude))
+idx <- grep("W", maps$Longitude)
+maps$Longitude <- gsub("E", "" , maps$Longitude)
+maps$Longitude <- gsub("W", "" , maps$Longitude)
---
+++ 210401044/Labex1_Q1_tests_210401044_munire_aksoy.R
@@ -0,0 +1,45 @@
+library("testthat")
+
+#2,2)
+rm(list = ls())  
+
+if (file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv")) {
+  file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")  
+}
+source("Labex1_Q1_210401044_munire_aksoy.R")
+
+test_that("MapsThatChangedOurWorld_StoryMap_Data.csv dosyası aktif dizinde mevcut olmalıdır", {
+  testthat::expect_true(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv"))
+})
+
+test_that("maps değişkeni Global Workspace'de mevcut olmalıdır", {
+  testthat::expect_true(exists("maps"))
+})
+
+test_that("maps değişkeni bir data.frame olmalıdır", {
+  testthat::expect_true(is.data.frame(maps))
+})
+
+test_that("maps data.frame'in ilk sütunu 'City' olmalıdır", {
+  testthat::expect_equal(names(maps)[1], "City")
+})
+
+test_that("maps data.frame'in 5. sütunu 'Title' içermelidir", {
+  testthat::expect_true(any(grepl("Title", colnames(maps))))
+})
+
+test_that("Latitude sütunu numeric değerlerden oluşmalıdır", {
+  testthat::expect_true(all(is.numeric(maps$Latitude)))
+})
+
+test_that("idx değişkeni Global Workspace'de mevcut olmalıdır", {
+  testthat::expect_true(exists("idx"))
+})
+
+test_that("idx değişkeni integer olmalıdır", {
+  testthat::expect_true(is.integer(idx))
+})
+
+test_that("Longitude sütunu numeric değerlerden oluşmalıdır", {
+  testthat::expect_true(all(is.numeric(maps$Longitude)))
+})
