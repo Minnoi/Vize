d62883d9b5afe3a5abd78fe1721ce10f2b142c6d
Minnoi <munireaksoy2002@gmail.com>
2023-11-16 17:59:50 +0300

    1.8 testleriyle tamamlandı.

---
+++ 210401044/Labex1_Q1_210401044_munire_aksoy.R
@@ -0,0 +1,15 @@
+library (readr)
+
+
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+dosya <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = dosya, method = "auto")
+maps <- read.csv(dosya, header=TRUE, sep=";")
+maps$Latitude <- as.numeric(gsub("N", "" , maps$Latitude))
+idx <- grep("W", maps$Longitude)
+maps$Longitude <- gsub("E", "" , maps$Longitude)
+maps$Longitude <- gsub("W", "" , maps$Longitude)
+maps$Year <- as.numeric(gsub("AD", "" , maps$Year))
+maps$Longitude <- as.numeric(maps$Longitude)
+maps$Latitude <- as.numeric(maps$Latitude)
+hist(maps$Year, breaks = 10, main = "Histogram Of Years", xlab = "Year", col = "lightblue")
